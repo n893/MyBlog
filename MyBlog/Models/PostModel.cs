614d85a2b26924e20b595d46a96c6139271bf3c6
n893 <kon0n@ukr.net>
2014-07-10 10:38:09 +0300

    PostModel added

---
+++ MyBlog/Models/PostModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlog.Models
+{
+	public class PostModel
+	{
+		[Key]
+		public int PostId { get; set; }
+		public string Name { get; set; }
+		public DateTime Date { get; set; }
+		public string Text { get; set; }
+	}
+}
